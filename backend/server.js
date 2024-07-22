7f93fca106676ee42ed5f6f79bc67bf8a7689779
axie22 <alexxie9667@gmail.com>
2024-07-22 09:53:01 -0400

    Finished setting up user auth

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const fsPromises = require("fs").promises;
 const todoDBName = "tododb";
 const useCloudant = true;
 
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
 
 
 //Init code for Cloudant
@@ -17,8 +25,10 @@ if (useCloudant)
     initDB();
 }
 
-
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000'
+}));
 app.use(bodyParser.json({ extended: true }));
 
 app.listen(port, () => console.log("Backend server live on " + port));
@@ -28,6 +38,28 @@ app.listen(port, () => console.log("Backend server live on " + port));
 app.get("/", (request, response) => {
     response.send({ message: "Connected to Backend server!" });
 });
+
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const upsertSucceeded = upsertUser(username, password)
+  res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('user');
+  res.end();
+});
+
+app.post("/items", cookieAuth, addItem);
+app.get("/items", cookieAuth, getItems);
+app.get("/items/search", cookieAuth, searchItems);
 
 //add new item to json file
 app.post("/add/item", addItem)
